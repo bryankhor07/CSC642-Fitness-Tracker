4f5027c4d0250b49b5e5844722b7ce11d44cfb46
Ishika <ishikaishu2000@gmail.com>
2024-05-04 10:17:37 -0700

    Added about and resources pages

--- Redirect.js
+++ Redirect.js
@@ -19,3 +19,36 @@ document.addEventListener("DOMContentLoaded", function() {
         window.location.href = 'LoginPage.html';
     });
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Get reference to the button
+    var logOutButton = document.getElementById('homeRed');
+
+    // Add click event listener to the button
+    logOutButton.addEventListener('click', function() {
+        // Redirect to the welcome page
+        window.location.href = 'WelcomePage.html';
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Get reference to the button
+    var logOutButton = document.getElementById('aboutRed');
+
+    // Add click event listener to the button
+    logOutButton.addEventListener('click', function() {
+        // Redirect to the welcome page
+        window.location.href = 'about.html';
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Get reference to the button
+    var logOutButton = document.getElementById('resourcesRed');
+
+    // Add click event listener to the button
+    logOutButton.addEventListener('click', function() {
+        // Redirect to the welcome page
+        window.location.href = 'resources.html';
+    });
+});
