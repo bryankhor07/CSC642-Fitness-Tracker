c7606d8969625c28bb2d3d751b6d752b62eaa555
nsingh166 <navjotsingh88888@gmail.com>
2024-05-09 22:41:37 -0700

    updated the exercise log page

--- Redirect.js
+++ Redirect.js
@@ -52,4 +52,68 @@ document.addEventListener("DOMContentLoaded", function() {
         window.location.href = 'resources.html';
     });
 
+    
+
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Get reference to the button
+    var logOutButton = document.getElementById('ExerciseLogRed');
+
+    // Add click event listener to the button
+    logOutButton.addEventListener('click', function() {
+        // Redirect to the welcome page
+        window.location.href = 'ExerciseLog.html';
+    });
+
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Get reference to the button
+    var logOutButton = document.getElementById('cardioRed');
+
+    // Add click event listener to the button
+    logOutButton.addEventListener('click', function() {
+        // Redirect to the welcome page
+        window.location.href = 'CardioPage.html';
+    });
+
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Get reference to the button
+    var logOutButton = document.getElementById('weightliftRed');
+
+    // Add click event listener to the button
+    logOutButton.addEventListener('click', function() {
+        // Redirect to the welcome page
+        window.location.href = 'Weightlifting.html';
+    });
+
+});
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Get reference to the button
+    var logOutButton = document.getElementById('mindandbodyRed');
+
+    // Add click event listener to the button
+    logOutButton.addEventListener('click', function() {
+        // Redirect to the welcome page
+        window.location.href = 'MindAndBody.html';
+    });
+
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Get reference to the button
+    var logOutButton = document.getElementById('othersRed');
+
+    // Add click event listener to the button
+    logOutButton.addEventListener('click', function() {
+        // Redirect to the welcome page
+        window.location.href = 'OthersPage.html';
+    });
+
 });
