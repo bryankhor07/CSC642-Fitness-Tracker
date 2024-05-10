772da5966dc2094b0a44d600fa3cc64440d987d4
Bryan Khor <bryankhor07@gmail.com>
2024-05-10 11:51:16 -0700

    Linked Exercise Log button to exercise page

--- Redirect.js
+++ Redirect.js
@@ -1,119 +1,120 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Get reference to the button
-    var logInButton = document.getElementById('logInBtn');
-    
-    // Add click event listener to the button
-    logInButton.addEventListener('click', function() {
-        // Redirect to the welcome page
-        window.location.href = 'WelcomePage.html';
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  // Get reference to the button
+  var logInButton = document.getElementById("logInBtn");
+
+  // Add click event listener to the button
+  logInButton.addEventListener("click", function () {
+    // Redirect to the welcome page
+    window.location.href = "WelcomePage.html";
+  });
 });
 
-document.addEventListener("DOMContentLoaded", function() {
-    // Get reference to the button
-    var logOutButton = document.getElementById('logOutBtn');
-    
-    // Add click event listener to the button
-    logOutButton.addEventListener('click', function() {
-        // Redirect to the welcome page
-        window.location.href = 'LoginPage.html';
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  // Get reference to the button
+  var logOutButton = document.getElementById("logOutBtn");
+
+  // Add click event listener to the button
+  logOutButton.addEventListener("click", function () {
+    // Redirect to the welcome page
+    window.location.href = "LoginPage.html";
+  });
 });
 
-document.addEventListener("DOMContentLoaded", function() {
-    // Get reference to the button
-    var logOutButton = document.getElementById('homeRed');
+document.addEventListener("DOMContentLoaded", function () {
+  // Get reference to the button
+  var logOutButton = document.getElementById("exerciseLog");
 
-    // Add click event listener to the button
-    logOutButton.addEventListener('click', function() {
-        // Redirect to the welcome page
-        window.location.href = 'WelcomePage.html';
-    });
+  // Add click event listener to the button
+  logOutButton.addEventListener("click", function () {
+    // Redirect to the welcome page
+    window.location.href = "ExerciseLog.html";
+  });
 });
 
-document.addEventListener("DOMContentLoaded", function() {
-    // Get reference to the button
-    var logOutButton = document.getElementById('aboutRed');
+document.addEventListener("DOMContentLoaded", function () {
+  // Get reference to the button
+  var logOutButton = document.getElementById("homeRed");
 
-    // Add click event listener to the button
-    logOutButton.addEventListener('click', function() {
-        // Redirect to the welcome page
-        window.location.href = 'about.html';
-    });
+  // Add click event listener to the button
+  logOutButton.addEventListener("click", function () {
+    // Redirect to the welcome page
+    window.location.href = "WelcomePage.html";
+  });
 });
 
-document.addEventListener("DOMContentLoaded", function() {
-    // Get reference to the button
-    var logOutButton = document.getElementById('resourcesRed');
+document.addEventListener("DOMContentLoaded", function () {
+  // Get reference to the button
+  var logOutButton = document.getElementById("aboutRed");
 
-    // Add click event listener to the button
-    logOutButton.addEventListener('click', function() {
-        // Redirect to the welcome page
-        window.location.href = 'resources.html';
-    });
-
-    
-
+  // Add click event listener to the button
+  logOutButton.addEventListener("click", function () {
+    // Redirect to the welcome page
+    window.location.href = "about.html";
+  });
 });
 
-document.addEventListener("DOMContentLoaded", function() {
-    // Get reference to the button
-    var logOutButton = document.getElementById('ExerciseLogRed');
+document.addEventListener("DOMContentLoaded", function () {
+  // Get reference to the button
+  var logOutButton = document.getElementById("resourcesRed");
 
-    // Add click event listener to the button
-    logOutButton.addEventListener('click', function() {
-        // Redirect to the welcome page
-        window.location.href = 'ExerciseLog.html';
-    });
-
+  // Add click event listener to the button
+  logOutButton.addEventListener("click", function () {
+    // Redirect to the welcome page
+    window.location.href = "resources.html";
+  });
 });
 
-document.addEventListener("DOMContentLoaded", function() {
-    // Get reference to the button
-    var logOutButton = document.getElementById('cardioRed');
+document.addEventListener("DOMContentLoaded", function () {
+  // Get reference to the button
+  var logOutButton = document.getElementById("ExerciseLogRed");
 
-    // Add click event listener to the button
-    logOutButton.addEventListener('click', function() {
-        // Redirect to the welcome page
-        window.location.href = 'CardioPage.html';
-    });
-
+  // Add click event listener to the button
+  logOutButton.addEventListener("click", function () {
+    // Redirect to the welcome page
+    window.location.href = "ExerciseLog.html";
+  });
 });
 
-document.addEventListener("DOMContentLoaded", function() {
-    // Get reference to the button
-    var logOutButton = document.getElementById('weightliftRed');
+document.addEventListener("DOMContentLoaded", function () {
+  // Get reference to the button
+  var logOutButton = document.getElementById("cardioRed");
 
-    // Add click event listener to the button
-    logOutButton.addEventListener('click', function() {
-        // Redirect to the welcome page
-        window.location.href = 'Weightlifting.html';
-    });
-
+  // Add click event listener to the button
+  logOutButton.addEventListener("click", function () {
+    // Redirect to the welcome page
+    window.location.href = "CardioPage.html";
+  });
 });
 
+document.addEventListener("DOMContentLoaded", function () {
+  // Get reference to the button
+  var logOutButton = document.getElementById("weightliftRed");
 
-
-document.addEventListener("DOMContentLoaded", function() {
-    // Get reference to the button
-    var logOutButton = document.getElementById('mindandbodyRed');
-
-    // Add click event listener to the button
-    logOutButton.addEventListener('click', function() {
-        // Redirect to the welcome page
-        window.location.href = 'MindAndBody.html';
-    });
-
+  // Add click event listener to the button
+  logOutButton.addEventListener("click", function () {
+    // Redirect to the welcome page
+    window.location.href = "Weightlifting.html";
+  });
 });
 
-document.addEventListener("DOMContentLoaded", function() {
-    // Get reference to the button
-    var logOutButton = document.getElementById('othersRed');
+document.addEventListener("DOMContentLoaded", function () {
+  // Get reference to the button
+  var logOutButton = document.getElementById("mindandbodyRed");
 
-    // Add click event listener to the button
-    logOutButton.addEventListener('click', function() {
-        // Redirect to the welcome page
-        window.location.href = 'OthersPage.html';
-    });
+  // Add click event listener to the button
+  logOutButton.addEventListener("click", function () {
+    // Redirect to the welcome page
+    window.location.href = "MindAndBody.html";
+  });
+});
 
+document.addEventListener("DOMContentLoaded", function () {
+  // Get reference to the button
+  var logOutButton = document.getElementById("othersRed");
+
+  // Add click event listener to the button
+  logOutButton.addEventListener("click", function () {
+    // Redirect to the welcome page
+    window.location.href = "OthersPage.html";
+  });
 });
